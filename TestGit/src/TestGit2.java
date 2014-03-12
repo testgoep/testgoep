bcb0dd69ea58dbdd31a034e6e970905ed2414217
rosaanna.napolitano@accenture.com <rosaanna.napolitano@accenture.com>
2014-03-12 14:47:36 +0100

    Prova con più files

--- TestGit/src/TestGit2.java
+++ TestGit/src/TestGit2.java
@@ -3,6 +3,7 @@ public class TestGit2 {
 
 	public static void main(String[] args) {
 		System.out.println("Ciao!");
+		System.out.println("Come va?");
 	}
 
 }
--- TestGit/src/TestGit3.java
+++ TestGit/src/TestGit3.java
@@ -3,6 +3,7 @@ public class TestGit3 {
 
 	public static void main(String[] args) {
 		System.out.println("Ciao fulvio come va?");
+		System.out.println("Ciao Rosanna come va?");
 	}
 
 }
