61f3daea89a096028ea5c7346af2ff7ab3899cd3
rosaanna.napolitano@accenture.com <rosaanna.napolitano@accenture.com>
2014-03-12 12:00:27 +0100

    Test3

--- TestGit/src/TestGit2.java
+++ TestGit/src/TestGit2.java
@@ -2,9 +2,8 @@
 public class TestGit2 {
 
 	public static void main(String[] args) {
-		System.out.println("Ciao!");
-		System.out.println("Ciao RosAAlba");
-
+		System.out.println("Ciao!");		
+		
 	}
 
 }
